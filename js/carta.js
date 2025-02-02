6698dcbae78329cfd6c5a6d1ce9cc0ba61e2bf91
JAntonioRobledoM <jantoniorobledomoreno@gmail.com>
2025-02-02 15:32:05 +0100

    Firts version

--- js/carta.js
+++ js/carta.js
@@ -8,6 +8,6 @@ function openCard() {
     setTimeout(function () {
         card.classList.add('open');
         paper.style.opacity = 1;
-        text.innerHTML = "Si has llegado hasta aquí te felicito, o eres muy listo o sabes algo de código. La web se irá actualizando cada cierto tiempo (el cuál también puedes descubrir en la web actualmente) con nuevos puntos secretos que esconden pistas. ¿Pistas de qué? Que sea el tiempo el que lo decida";
+        text.innerHTML = "Si has llegado hasta aquí te felicito, o eres muy listo o sabes algo de código. La web se irá actualizando cada cierto tiempo con nuevos puntos secretos que esconden pistas. ¿Pistas de qué? Que sea el tiempo el que lo decida";
     }, 100);
 }
